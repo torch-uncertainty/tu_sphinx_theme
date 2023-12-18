02b6ee7842126a1547bd3b0fe840b4da50388de5
Olivier Laurent <olivier.ar.laurent@gmail.com>
2023-12-18 15:39:42 +0100

    :shirt: Reformat and add version

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 from setuptools import setup
-from io import open
 from tu_sphinx_theme import __version__
 
 # Taken from tu_sphinx_theme
 setup(
-    name = 'tu_sphinx_theme',
-    version =__version__,
-    author = 'TorchUncertainty Contributors',
-    author_email= 'olivier.laurent@ensta-paris.fr',
+    name="tu_sphinx_theme",
+    version=__version__,
+    author="TorchUncertainty Contributors",
+    author_email="olivier.laurent@ensta-paris.fr",
     url="https://github.com/torch-uncertainty/tu_sphinx_theme",
     docs_url="https://github.com/torch-uncertainty/tu_sphinx_theme",
-    description='TorchUncertainty Sphinx Theme',
-    py_modules = ['tu_sphinx_theme'],
-    packages = ['tu_sphinx_theme'],
+    description="TorchUncertainty Sphinx Theme",
+    py_modules=["tu_sphinx_theme"],
+    packages=["tu_sphinx_theme"],
     include_package_data=True,
     zip_safe=False,
-    package_data={'tu_sphinx_theme': [
-        'theme.conf',
-        '*.html',
-        'static/css/*.css',
-        'static/js/*.js',
-        'static/js/*/*.js',
-        'static/fonts/*/*.*',
-        'static/images/*.*',
-        'theme_variables.jinja'
-    ]},
-    entry_points = {
-        'sphinx.html_themes': [
-            'tu_sphinx_theme = tu_sphinx_theme',
+    package_data={
+        "tu_sphinx_theme": [
+            "theme.conf",
+            "*.html",
+            "static/css/*.css",
+            "static/js/*.js",
+            "static/js/*/*.js",
+            "static/fonts/*/*.*",
+            "static/images/*.*",
+            "theme_variables.jinja",
         ]
     },
-    license= 'MIT License',
+    entry_points={
+        "sphinx.html_themes": [
+            "tu_sphinx_theme = tu_sphinx_theme",
+        ]
+    },
+    license="MIT License",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
@@ -40,9 +41,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Internet",
-        "Topic :: Software Development :: Documentation"
+        "Topic :: Software Development :: Documentation",
     ],
-    install_requires=[
-       'sphinx'
-    ]
+    install_requires=["sphinx"],
 )
--- tu_sphinx_theme.py
+++ tu_sphinx_theme.py
@@ -1,6 +1,11 @@
 from os import path
+
 package_dir = path.dirname(path.abspath(__file__))
 template_path = path.join(package_dir)
 
+
 def get_path():
     return template_path
+
+
+__version__ = "0.0.1"
